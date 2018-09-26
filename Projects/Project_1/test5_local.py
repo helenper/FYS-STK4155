17ed34066a4ca8be0e118248a6e8065a08b3de81
monaaanderssen <mona_anderssen@hotmail.com>
2018-09-26 18:11:38 +0200

    Added lasso and started on bootstrap

--- Projects/Project_1/test5_local.py
+++ Projects/Project_1/test5_local.py
@@ -10,7 +10,6 @@
 ####################
 
 # Import necessary packages
-
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -20,6 +19,8 @@ from random import random, seed
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import mean_squared_error, r2_score
+from time import time
+import matplotlib.mlab as mlab
 
 
 def FrankeFunction(x,y):
@@ -36,6 +37,9 @@ n = 10							# number of datapoints
 x = np.random.uniform(0.0,1.0, n*n)       # create a random number for x-values in dataset
 y = np.random.uniform(0.0,1.0, n*n)       # create a random number for y-values in dataset with noise
 
+#x = np.sort(x)
+#y = np.sort(y)
+
 num_rows = n 	# Choose this to be number of x points
 num_cols = n 	# Choose this to be number of y points
 
@@ -50,28 +54,10 @@ X = np.c_[np.ones((num_rows*num_cols,1)) , x, y, x**2, x*y, y**2, \
 
 
 z = FrankeFunction(x, y) + 0.9*np.random.randn(1) # z with noise
-print(np.shape(z))
 
 beta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(z) 
-print(np.shape(beta))
 
-
-"""
-zpredict = np.zeros((len(x), len(y)))
-for i in range(len(x)):
-    for j in range(len(y)):
-        x_value = x[i]
-        y_value = y[j]
-        zpredict[j,i] = np.array([1 , x_value, y_value, x_value**2, x_value*y_value, y_value**2, \
-
-                x_value**3, x_value**2*y_value, x_value*y_value**2, y_value**3, \
-
-                x_value**4, x_value**3*y_value, x_value**2*y_value**2, x_value*y_value**3,y_value**4, \
-
-                x_value**5, x_value**4*y_value, x_value**3*y_value**2, x_value**2*y_value**3,x_value*y_value**4, y_value**5]).dot(beta)
-print(np.shape(zpredict))
-"""
-zpredict = X.dot(beta) #Is this correct? or should we do as above?
+zpredict = X.dot(beta) 
 
 # The mean squared error  
 
@@ -100,16 +86,16 @@ def R_2(y, y_tilde):
 
 R2 = 1- (np.sum((z-zpredict)**2))/(np.sum((z-np.mean(z))**2))
 
-print('Variance score easy: %.5f' % R2)
-print('Variance score: %.5f' % R_2(z, zpredict))
-print('Variance score scitkitlearn: %.5f' % r2_score(z, zpredict))
-"""
+print('R2 score easy: %.5f' % R2)
+print('R2 score: %.5f' % R_2(z, zpredict))
+print('R2 score scitkitlearn: %.5f' % r2_score(z, zpredict))
+
 #Some other variances:
 var=1.0/z.shape[0] *np.sum((z - np.mean(z))**2)
-betavar=1.0/z.shape[0] *np.sum((beta - np.mean(beta))**2)
-print('Variance:', var)
-print('Variance of beta', betavar)
-"""
+#betavar=1.0/z.shape[0] *np.sum((beta - np.mean(beta))**2)
+print('Variance: %.5f'% var)
+#print('Variance of beta', betavar)
+
 
 # Ridge and Lasso:
 np.random.seed(4155)
@@ -164,24 +150,188 @@ for i in range(num_values):
 #Lasso:
 
 
+lasso=Lasso(alpha=0.001)
+lasso.fit(X,z)
+predl=lasso.predict(X)
+print("Lasso Coefficient: ", lasso.coef_)
+print("Lasso Intercept: ", lasso.intercept_)
+print("R2 score:", r2_score(z,predl))
 
-#Split into training and test
-x_train=x[:37, np.newaxis]
-x_test=x[37:, np.newaxis]
-y_train=y[:37, np.newaxis]
-y_test=y[37:, np.newaxis]
-z_train=z[:37]
-z_test=z[37:]
-
-
+"""
+plt.scatter(X,z,color='green', label="Training Data")
+plt.plot(X, predl, color='blue', label="Lasso")
+plt.legend()
+plt.show()
+"""
+"""
 print(np.shape(x_train))
 poly5 = PolynomialFeatures(5)
 #X_train = poly5.fit_transform()
+
+X_train = np.c_[x_train,y_train]
+X_test = np.c_[x_test, y_test]
 lasso=Lasso(alpha=0.1)
 lasso.fit(X_train,z_train)
 predl=lasso.predict(z_test)
 print("Lasso Coefficient: ", lasso.coef_)
 print("Lasso Intercept: ", lasso.intercept_)
+"""
+
+
+# Plotting:
+# Importing necessary packages for plotting
+
+
+def plotFrankeFunction(x, y, z, type):
+    #x = np.sort(x)
+    #y = np.sort(y)
+    x, y = np.meshgrid(x, y)
+    #z = z.reshape(-1,1)
+    print(x.shape)
+    print(z.shape)
+    z = FrankeFunction(x,y)
+    #n,m = x.shape 
+    #z = z.reshape(n,m)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    if type==1:
+        plt.title('Franke function with actual z')
+    elif type==2:
+        plt.title('Franke function with our prediction of z')
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+    # Customize the z axis.
+    #ax.set_zlim(-0.10, 1.40)
+    #ax.zaxis.set_major_locator(LinearLocator(10))
+    #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f')) 
+    return surf
+
+# Plotting Franke function with actual z:
+#plotFrankeFunction(x, y, z, type=1)
+
+# Plotting Franke function with our prediction of z:
+#plotFrankeFunction(x, y, zpredict, type=2)
+"""
+# Plotting Ridge:
+def plotRidge(x,y):
+    # Sorting
+    sort_ind = np.argsort(x[:,0])
+
+    x_plot = x[sort_ind,0]
+    x_centered_plot = x_[sort_ind,0]
+
+    pred_ls_plot = pred_ls[sort_ind,0]
+    pred_ridge_plot = pred_ridge[sort_ind,:]
+    pred_ridge_centered_plot = pred_ridge_centered[sort_ind,:]
+
+    # Plott not centered
+    plt.plot(x_plot,pred_ls_plot,label='ls')
+
+    for i in range(num_values):
+        plt.plot(x_plot,pred_ridge_plot[:,i],label='ridge, lmb=%g'%lmb_values[i])
+
+    plt.plot(x,y,'ro')
+
+    plt.title('linear regression on un-centered data')
+    plt.legend()
+
+    # Plott centered
+    plt.figure()
+
+    for i in range(num_values):
+        plt.plot(x_centered_plot,pred_ridge_centered_plot[:,i],label='ridge, lmb=%g'%lmb_values[i])
+
+    plt.plot(x_,y,'ro')
+
+    plt.title('linear regression on centered data')
+    plt.legend()
+
+
+    # 2.
+
+    pred_ridge_scikit =  np.zeros((n_samples,num_values))
+    for i,lmb in enumerate(lmb_values):
+        pred_ridge_scikit[:,i] = (Ridge(alpha=lmb,fit_intercept=False).fit(X,y).predict(X)).flatten() # fit_intercept=False fordi bias er allerede i X
+
+    plt.figure()
+
+    plt.plot(x_plot,pred_ls_plot,label='ls')
+
+    for i in range(num_values):
+        plt.plot(x_plot,pred_ridge_scikit[sort_ind,i],label='scikit-ridge, lmb=%g'%lmb_values[i])
+
+    plt.plot(x,y,'ro')
+    plt.legend()
+    plt.title('linear regression using scikit')
+
+    plt.show()
+    return 
+
+plotRidge(x, y)
+"""
+
+"""
+x_train=x[:37, np.newaxis]
+x_test=x[37:, np.newaxis]
+y_train=y[:37, np.newaxis]
+y_test=y[37:, np.newaxis]
+z_train=z[:37, np.newaxis]
+z_test=z[37:, np.newaxis]
+
+def stat(data):
+    return np.mean(data)
+
+def Bootstrap(data, statistics, N):
+    t = np.zeros(N)
+    inds = np.arange(len(data))
+    t0 = time()
+
+    for i in range(N):
+        t[i] = statistics(data[np.random.randint(0, len(data), len(data))])
+
+    print("Runtime: %g sec" % (time()-t0)); print("Bootstrap Statistics :")
+    print("original           bias      std. error")
+    print("%8g %8g %14g %15g" % (statistics(data), np.std(data), np.mean(t), np.std(t)))
+
+    return t
+
+mu, sigma = 100, 15
+datapoints = 10000
+
+t = Bootstrap(X, stat, datapoints)
+
+n, binsboot, patches = plt.hist(t,50,normed=1,facecolor='green', alpha=0.75)
+
+y = mlab.normpdf(binsboot, np.mean(t), np.std(t))
+#lt = plt.plot(binsboot, z, 'g--', linewidth=1)
+
+plt.show()
+"""
+
+
+
+def bootstrap(data, percent):
+    size = percent*len(data)
+    train = np.random.choice(len(data),int(size))
+    test = set(range(len(data)) - set(train))
+    yield train, test
+
+X_train = np.zeros(len(X))
+X_test = np.zeros(len(X))
+for train_index, test_index in bootstrap(X,0.7):
+    X_train[train_index] = X[train_index]
+    X_test[test_index] = X[test_index]
+
+
+
 
 
 
