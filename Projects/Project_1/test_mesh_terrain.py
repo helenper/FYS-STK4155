32588c45d4915f9b53c768812a8c0dff0dac44c6
monaaanderssen <mona_anderssen@hotmail.com>
2018-10-05 14:35:56 +0200

    added real data

--- Projects/Project_1/test_mesh_terrain.py
+++ Projects/Project_1/test_mesh_terrain.py
@@ -7,6 +7,7 @@
 from functions import *
 from plotfunctions import *
 from imageio import imread
+from sklearn.model_selection import train_test_split
 
 #-------------------------------------------------
 # Define a seed to use while testing the program. 
@@ -28,7 +29,7 @@ noise = 0.1								# strengt of noise
 # Load the terrain
 #terrain1 = imread('terrainone.tif')
 #x, y = imread('terrainone.tif')
-terrain1 = imread('/Users/monaanderssen/Documents/Master/1.semester/FYS-STK4155/FYS-STK4155-git/Projects/Project_1/terrainone.tif')
+terrain1 = imread('terrainone.tif')
 [n,m] = terrain1.shape
 
     ## Find some random patches within the dataset and perform a fit
@@ -43,7 +44,7 @@ cols = np.linspace(0,1,patch_size_col)
 [C,R] = np.meshgrid(cols,rows)
 
 #print('hei', np.size(C), np.size(R))
-"""
+
 x = C.reshape(-1,1)
 y = R.reshape(-1,1)
 print(len(x))
@@ -53,7 +54,7 @@ num_data = patch_size_row*patch_size_col
 
     # Find the start indices of each patch
 
-num_patches = 2
+num_patches = 5
 
 np.random.seed(4155)
 
@@ -73,62 +74,13 @@ for i,row_start, col_start in zip(np.arange(num_patches),row_starts, col_starts)
 
 	z = patch.reshape(-1,1)
 	
-	X = polynomialfunction(x,y,len(x),5)
+	X = polynomialfunction(x,y,len(x),4)
 
 	X_train, X_test, z_train, z_test = train_test_split(X, z, train_size = 0.7)
-"""
 
-"""
-	x_patch = [i + 1 for i in range(patch_size_row)] + row_start
-	y_patch = [i + 1 for i in range(patch_size_col)] + col_start
-	
+	mse_OLS, r2_score_OLS, bias_OLS, var_OLS = lasso(X_train,z_train, X_test, z_test, 0.01)
 
-	patch = terrain1[row_start:row_end, col_start:col_end]
-	z = patch.reshape(-1,1)
-	#print(row_start, row_end)
-	print(np.shape(patch), np.shape(z))
-	#print(len(patch))
-	N = terrain1[row_start:row_end]
-	train_indices, test_indices = splitdata(z, 0.7)
-	#x_train_indices, x_test_indices = splitdata(x_patch, 0.7)
-	#y_train_indices, y_test_indices = splitdata(y_patch, 0.7)
-	#print(np.shape(train_indices), np.shape(test_indices))
-	#print(len(train_indices))
-	#from sklearn.model_selection import train_test_split
-	#X = polynomialfunction(x, y, len(x), degree=5)
-	#X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2)
-	
-	x_train_orig = x[train_indices]
-	x_test = x[test_indices]
-	y_train_orig = y[train_indices]
-	y_test = y[test_indices]
-
-	z_train = z[train_indices]
-	z_test = z[test_indices]
-	
-	#z_train = patch_train.reshape(-1,1)
-	#z_test = patch_test.reshape(-1,1)
-	#print(np.shape(x_train_orig), np.shape(y_train_orig), np.shape(z_train), num_data)
-	
-	X_train = polynomialfunction(x_train_orig, y_train_orig, len(x_train_orig), degree=2)
-	X_test = polynomialfunction(x_test, y_test, len(x_test), degree=2)
-
-	#print(np.shape(X_train), np.shape(z_train), np.shape(X_test), np.shape(z_test))
-
-	mse , R2, bias, var, beta = OLS(X_train, z_train, X_test, z_test)
-
-	print('mse',mse, 'R2',R2, 'bias',bias,'var', var)
-
-	alpha = 0.001
-	mse_R , R2_R, bias_R, var_R = ridge(X_train, z_train, X_test, z_test, alpha, write=0)
-	
-	print('Rigde:', 'mse',mse_R, 'R2',R2_R, 'bias',bias_R,'var', var_R)
-
-
-	mse_L , R2_L, bias_L, var_L = lasso(X_train,z_train,X_test,z_test,alpha, write=0)
-
-	print('Lasso:', 'mse',mse_L, 'R2',R2_L, 'bias',bias_L,'var', var_L)
-"""
+	print(mse_OLS, r2_score_OLS)
 
 #---------------------------------------------------------
 
@@ -145,7 +97,7 @@ for i,row_start, col_start in zip(np.arange(num_patches),row_starts, col_starts)
 # Use bootstrap to define train and test data and calculate a mean 
 # value for MSE and R2 for the different methods OSL, Ridge and Lasso
 #---------------------------------------------------------------------
-"""
+
 iterations = 10		# number of times we split and save our calculations in train and test point
 
 # Create arrays to hold different values to be taken mean over later. 
@@ -179,25 +131,25 @@ beta_file = open('beta_data_terrain.txt', 'w')
 for a in alpha:
 	file.write('%f   \n' %a)
 	for i in range(iterations):
-	    x_train, y_train = bootstrap(x_train_orig,y_train_orig)
-	    z_train = FrankeFunction(x_train, y_train) + noise*np.random.randn(len(x_train)) # z with noise
+	    X_train, z_train = bootstrap(X_train,z_train)
+	    #z_train = FrankeFunction(x_train, y_train) + noise*np.random.randn(len(x_train)) # z with noise
 
-	    for j in range(5):
-	        X_train = polynomialfunction(x_train,y_train,len(x_train),degree=(j+1))
-	        X_test = polynomialfunction(x_test,y_test,len(x_test),degree=(j+1))
+	    #for j in range(5):
+	    #    X_train = polynomialfunction(x_train,y_train,len(x_train),degree=(j+1))
+	    #    X_test = polynomialfunction(x_test,y_test,len(x_test),degree=(j+1))
 	        
 
-	        mse_OLS[j][i], r2score_OLS[j][i], bias_OLS[j][i], var_OLS[j][i], beta = OLS(X_train,z_train, X_test, z_test)
+	    mse_OLS[i], r2score_OLS[i], bias_OLS[i], var_OLS[i], beta = OLS(X_train,z_train, X_test, z_test)
 	        #print(np.shape(beta))
 
-	        betaConfidenceInterval(beta, beta_file)
+	    #betaConfidenceInterval(beta, beta_file)
 
-	        mse_Ridge[j][i], r2score_Ridge[j][i], bias_Ridge[j][i], var_Ridge[j][i] = ridge(X_train,z_train,X_test,z_test,a, write=0)
+	    #mse_Ridge[j][i], r2score_Ridge[j][i], bias_Ridge[j][i], var_Ridge[j][i] = ridge(X_train,z_train,X_test,z_test,a, write=0)
 
-	        mse_Lasso[j][i], r2score_Lasso[j][i], bias_Lasso[j][i], var_Lasso[j][i] = lasso(X_train,z_train,X_test,z_test,a, write=0)
+	    #mse_Lasso[j][i], r2score_Lasso[j][i], bias_Lasso[j][i], var_Lasso[j][i] = lasso(X_train,z_train,X_test,z_test,a, write=0)
 
-
-	
+	    print(mse_OLS[i], r2score_OLS[i])
+"""
 	mse_OLS_average1 = np.mean(mse_OLS[0])	
 	mse_OLS_average2 = np.mean(mse_OLS[1]) 
 	mse_OLS_average3 = np.mean(mse_OLS[2]) 
@@ -291,12 +243,13 @@ for a in alpha:
 file.close()
 beta_file.close()
 """
+"""
 #print(np.size(interval))
 #print('hei')
 #betaConfidenceInterval(beta)
 #plotMSE()
 """
-
+"""
 print("Datasize = ", n, "\n")
 print("alpha = ", alpha, "\n")
 print("bootstrap-iterations = ", iterations, "\n")
@@ -326,3 +279,54 @@ print("1. order: ", r2score_Lasso_average1, "2. order: ", r2score_Lasso_average2
 
 #var=1.0/z.shape[0] *np.sum((z - np.mean(z))**2)
 #print(var)
+"""
+	x_patch = [i + 1 for i in range(patch_size_row)] + row_start
+	y_patch = [i + 1 for i in range(patch_size_col)] + col_start
+	
+
+	patch = terrain1[row_start:row_end, col_start:col_end]
+	z = patch.reshape(-1,1)
+	#print(row_start, row_end)
+	print(np.shape(patch), np.shape(z))
+	#print(len(patch))
+	N = terrain1[row_start:row_end]
+	train_indices, test_indices = splitdata(z, 0.7)
+	#x_train_indices, x_test_indices = splitdata(x_patch, 0.7)
+	#y_train_indices, y_test_indices = splitdata(y_patch, 0.7)
+	#print(np.shape(train_indices), np.shape(test_indices))
+	#print(len(train_indices))
+	#from sklearn.model_selection import train_test_split
+	#X = polynomialfunction(x, y, len(x), degree=5)
+	#X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2)
+	
+	x_train_orig = x[train_indices]
+	x_test = x[test_indices]
+	y_train_orig = y[train_indices]
+	y_test = y[test_indices]
+
+	z_train = z[train_indices]
+	z_test = z[test_indices]
+	
+	#z_train = patch_train.reshape(-1,1)
+	#z_test = patch_test.reshape(-1,1)
+	#print(np.shape(x_train_orig), np.shape(y_train_orig), np.shape(z_train), num_data)
+	
+	X_train = polynomialfunction(x_train_orig, y_train_orig, len(x_train_orig), degree=2)
+	X_test = polynomialfunction(x_test, y_test, len(x_test), degree=2)
+
+	#print(np.shape(X_train), np.shape(z_train), np.shape(X_test), np.shape(z_test))
+
+	mse , R2, bias, var, beta = OLS(X_train, z_train, X_test, z_test)
+
+	print('mse',mse, 'R2',R2, 'bias',bias,'var', var)
+
+	alpha = 0.001
+	mse_R , R2_R, bias_R, var_R = ridge(X_train, z_train, X_test, z_test, alpha, write=0)
+	
+	print('Rigde:', 'mse',mse_R, 'R2',R2_R, 'bias',bias_R,'var', var_R)
+
+
+	mse_L , R2_L, bias_L, var_L = lasso(X_train,z_train,X_test,z_test,alpha, write=0)
+
+	print('Lasso:', 'mse',mse_L, 'R2',R2_L, 'bias',bias_L,'var', var_L)
+"""
