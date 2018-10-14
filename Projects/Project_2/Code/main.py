8beb065d188cfc73726ced0fdf641b37cd3fc915
Simon Millerjord <simonmillerjord@gmail.com>
2018-10-14 16:48:31 +0200

    Calling the right function to run OLS, Ridge and Lasso for the OneDim ising model. Might have to remove the file-writing as this is done in functions.py

--- Projects/Project_2/Code/main.py
+++ Projects/Project_2/Code/main.py
@@ -10,26 +10,51 @@ from functions import *
 import sys
 import numpy as np
 import scipy.sparse as sp
-np.random.seed(12)
 import warnings
 
 #Comment this to turn on warnings
 warnings.filterwarnings('ignore')
-### define Ising model aprams
+
+np.random.seed(12)
+
 # system size
 L=40
-# create 10000 random Ising states
+iterations = 100
+methods = ['OLS', 'Ridge', 'Lasso']
+lambda_values = [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1]
+dim = input('Which dimension do you want to run for? If one => write 1 or one, if two => write 2 or two.')
+
+if dim == '1' or dim == 'one':
+	for m in methods:
+		print('The next method to run is: %s' %m)
+		answer = input('Do you want to run for %s [y/n] ?' %m)
+		if answer == 'y' or answer == 'Y':
+			#file = open('results_OneDim_%s.txt' %m,  'w')
+			mse_average, r2score_average, bias_average, var_average, beta, mse_min, R2_for_Min_MSE_value = OneDim(L, iterations, lambda_values, method=m)
+
+			"""
+			file.write('The results from running with lamda = %f \n' % lambda_values)
+			file.write('MSE_average:        %f \n' %mse_average )
+			file.write('R2_score_average:   %f \n' %r2score_average)
+			file.write('Bias_average:       %f \n' %bias_average)
+			file.write('Variance_average:   %f \n' %var_average)
+			file.write('Min_MSE_value:      %f \n' %mse_min)
+			file.write('R2_for_Min_MSE_value:       %f \n' %r2_min)
+			file.write('The_best_beta_parameters:   %s \n' %the_beta_values)
+			file.write('\n')
+			file.close()
+			"""
+		if answer == 'n' or answer == 'N':
+			print('Moving on')
+
+
+#elif dim == '2' or dim == 'two':
+
+
+else:
+	print("Must be 1 or 2 dimensions.")
+
 states=np.random.choice([-1, 1], size=(10000,L))
-def ising_energies(states,L):
-    """
-    This function calculates the energies of the states in the nn Ising Hamiltonian
-    """
-    J=np.zeros((L,L),)
-    for i in range(L):
-        J[i,(i+1)%L]-=1.0
-    # compute energies
-    E = np.einsum('...i,ij,...j->...',states,J,states)
-    return E
-# calculate Ising energies
 energies=ising_energies(states,L)
+
 
