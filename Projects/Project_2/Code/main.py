12cbc3463b6250cb60a4a3573c44512e54f522fb
Simon Millerjord <simonmillerjord@gmail.com>
2018-10-17 16:46:10 +0200

    Reading data for 2D is implemented. Should still take a look at the OneDim case and see what is needed.

--- Projects/Project_2/Code/main.py
+++ Projects/Project_2/Code/main.py
@@ -11,6 +11,8 @@ import sys
 import numpy as np
 import scipy.sparse as sp
 import warnings
+import pickle
+import os
 
 #Comment this to turn on warnings
 warnings.filterwarnings('ignore')
@@ -48,12 +50,48 @@ if dim == '1' or dim == 'one':
 			print('Moving on')
 
 
-methods = []
+
 
 elif dim == '2' or dim == 'two':
-	for m in methods:
-		print('The next method to run is: %s' %m)
-		
+	num_classes = 2
+	train_to_test_ratio = 0.7 # Training samples
+
+	# path to data directory
+	path_to_data = 'IsingData/'
+	file_name = "Ising2DFM_reSample_L40_T=All.pkl" # This file contains 16*10000 samples taken in the T=0.25 to T=4.00 temp range
+	
+	data = pickle.load(open(path_to_data+file_name,'rb')) # pickle reads the file, and returns the Python object (1D array,compressed bits)
+	data = np.unpackbits(data).reshape(-1,1600) # Decompress array and reshape for convenience as a 40x40 lattice
+	data = data.astype('int')
+	data[np.where(data==0)] = -1 # map 0 state to -1 (Ising variable can take values +/-1)
+	
+	file_name = "Ising2DFM_reSample_L40_T=All_labels.pkl" # This file contains 16*10000 samples taken in the above range
+	labels = pickle.load(open(path_to_data+file_name,'rb'))
+
+	# divide data into ordered, critical and disordered
+	X_ordered = data[:70000,:]
+	Y_ordered = labels[:70000]
+
+	X_critical = data[70000:100000,:]
+	Y_critical = labels[70000:100000]
+
+	X_disordered = data[100000:,:]
+	Y_disordered = labels[100000:]
+
+	del data, labels
+
+	# Define training and test data sets
+	X = np.concatenate((X_ordered,X_disordered))
+	Y = np.concatenate((Y_ordered,Y_disordered))
+
+	X_train, X_test, Y_train, Y_test, = train_test_split(X,Y,train_size = train_to_test_ratio)
+
+
+
+	twodim = TwoDim(X_train,X_test,Y_train,Y_test)
+#	for m in methods:
+#		print('The next method to run is: %s' %m)
+
 
 
 else:
