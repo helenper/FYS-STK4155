fixed gradient descent without network.

Projects/Project_2/Code/functions.py
@@ -231,22 +231,23 @@ def sigmoid(X, Y):
 
 def TwoDim(X_train, X_test, Y_train, Y_test, NN, num_classes, m):
 
-    Niterations = 1000
-    beta = 1e-3*np.random.randn(1600)
-
+    Niterations = 10
+    beta = 1e-6*np.random.randn(1600)
+    print(X_train.shape, beta.shape)
+    
     if NN == 'y':
         
         Neural_Network_TwoDim(X_train, Y_train, X_test, Y_test, m)
 
     else:
-        eta = 0.01
+        eta = 0.001
         for i in range(Niterations):
             p1 = 1./(1+np.exp(-X_train @ beta)) #theta = beta
             p0 = 1 - p1
-            p = np.choose(Y_train, [p0,p1])
-            dC = -X_train.T @ (Y_train - p1)
+            #p = np.choose(Y_train, [p0,p1])
+            dC = np.matmul(-X_train.T,  (Y_train - p1)) / len(Y_train)
             beta = beta - dC*eta # beta is the same as weights in one dim.
-            correct = p >= 0.5
+            #correct = p >= 0.5
             Error = p1 - Y_train
             Accuracy(Error)
 