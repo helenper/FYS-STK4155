c3b1d8c6e6c8410e81d514c3ddb154e1a84e59c1
Simon Millerjord <simonmillerjord@gmail.com>
2018-11-11 18:22:53 +0100

    feilsokt litt.

--- Projects/Project_2/Code/Neural_Network_OneDim.py
+++ Projects/Project_2/Code/Neural_Network_OneDim.py
@@ -10,21 +10,22 @@ def sigmoid(z):
 
 
 def feed_forward_OneDim(X_train, weights_hidden, bias_hidden, weights_output, bias_output):
-	print("HOT0!")
+	
 	# weighted sum of inputs to the hidden layer
 	z_hidden = np.matmul(X_train,weights_hidden) + bias_hidden #hidden layer
 	activation_hidden = sigmoid(z_hidden) #Sigmoid layer hidden
-	print("HOT1!")
+	
 	z_output = np.matmul(activation_hidden, weights_output) + bias_output #layer
 	activation_output = sigmoid(z_output) # Sigmoid layer 
-	print("HOT2!")
+	
 	return activation_hidden, activation_output, z_output
 
 
 def backwardpropagation(X_train,E_train, weights_hidden, bias_hidden, weights_output, bias_output):
 	activation_hidden, activation_output, z_output = feed_forward_OneDim(X_train, weights_hidden, bias_hidden, weights_output, bias_output)
-	error_output = z_output - E_train.reshape(-1,1)# * activation_output * (1 - activation_output)
 
+	error_output = (z_output - E_train.reshape(-1,1))# * activation_output * (1 - activation_output)
+	
 	error_hidden = np.matmul(error_output,weights_output.T) * activation_hidden * (1 - activation_hidden)
 
 	output_gradient_weights = np.matmul(activation_hidden.T, error_output)
@@ -33,12 +34,13 @@ def backwardpropagation(X_train,E_train, weights_hidden, bias_hidden, weights_ou
 	hidden_gradient_weights = np.matmul(X_train.T, error_hidden)
 	hidden_gradient_bias = np.sum(error_hidden, axis=0)
 
-	return output_gradient_weights, output_gradient_bias, 	hidden_gradient_weights, hidden_gradient_bias
+	return output_gradient_weights, output_gradient_bias, hidden_gradient_weights, hidden_gradient_bias
 
 
 def Neural_Network_OneDim(X_train, E_train, eta, lmbd=1):
+	np.random.seed(12)
 	n_inputs, n_features = X_train.shape
-	n_h_neurons = 50
+	n_h_neurons = 100
 	n_categories = 1
 
 	weights_hidden = np.random.randn(n_features, n_h_neurons)
@@ -47,17 +49,19 @@ def Neural_Network_OneDim(X_train, E_train, eta, lmbd=1):
 	weights_output = np.random.randn(n_h_neurons, n_categories)
 	bias_output = np.zeros(n_categories) + 0.01
 	
+	batch = 200
 	
-	for i in range(1000):
+	for i in range(10000):
 		# calculate gradients
-		dWo, dBo, dWh, dBh = backwardpropagation(X_train, E_train, weights_hidden, bias_hidden, weights_output, bias_output)
+		index = np.random.randint(len(X_train), size=batch)
+		dWo, dBo, dWh, dBh = backwardpropagation(X_train[index], E_train[index], weights_hidden, bias_hidden, weights_output, bias_output)
 
 		#update weights and biases
 		weights_output -= eta * dWo
 		weights_hidden -= eta * dWh
 		bias_output -= eta * dBo
 		bias_hidden -= eta * dBh
-
+		#print(weights_output, weights_hidden)
 	return weights_output, weights_hidden, bias_output, bias_hidden
 
 
