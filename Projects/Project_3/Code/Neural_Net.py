f326ee31e354a910e83eb01c938445dc12067b4e
Simon Millerjord <simonmillerjord@gmail.com>
2018-11-25 17:03:35 +0100

    Added a forloop that makes the layers in the network.

--- Projects/Project_3/Code/Neural_Net.py
+++ Projects/Project_3/Code/Neural_Net.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 
+
+def Network(X, num_layers, num_nodes):
+
+	model = tf.keras.Sequential()
+
+	for i in range(num_layers):
+		model.add(tf.keras.layers.Dense(num_nodes[i], activation='relu', input_dim=X.shape[1]))
+		model.add(tf.keras.layers.Dropout(0.2))
 
 
