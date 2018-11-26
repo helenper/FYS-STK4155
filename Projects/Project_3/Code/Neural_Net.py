68451e5b3d29cd46fa99164025b17f6da29102d4
Simon Millerjord <simonmillerjord@gmail.com>
2018-11-26 17:52:54 +0100

    Changed the loss and optimier

--- Projects/Project_3/Code/Neural_Net.py
+++ Projects/Project_3/Code/Neural_Net.py
@@ -3,22 +3,24 @@ import numpy as np
 import tensorflow as tf
 
 
-def Network(X, y, num_layers, num_nodes):
+def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers, num_nodes):
 
 	model = tf.keras.Sequential()
 
-	model.add(tf.keras.layers.Dense(num_nodes[i], activation='relu', input_dim=X.shape[1]))
+	model.add(tf.keras.layers.Dense(num_nodes, activation='relu', input_dim=X_train.shape[1]))
 	model.add(tf.keras.layers.Dropout(0.2))
 	
 	for i in range(num_layers):
-		model.add(tf.keras.layers.Dense(num_nodes[i], activation='relu'))
+		model.add(tf.keras.layers.Dense(num_nodes, activation='relu'))
 		model.add(tf.keras.layers.Dropout(0.2))
 
-	model.add(tf.keras.layers.Dense(y.shape[1], activation='softmax'))
+	model.add(tf.keras.layers.Dense(y_train.shape[1], activation='softmax'))
 
 
-	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+	model.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
 
 
-	model.fit(X,y,epochs=100,batch_size=32,validation_data=[])
+	model.fit(X_train,y_train,epochs=100,batch_size=32,validation_data=[X_validate,y_validate])
 
+
+	print('Accuracy: ', model.evaluate(X_test, y_test))
