86d0d97b45f724a551ddc671c6191db0fcca4d84
Simon Millerjord <simonmillerjord@gmail.com>
2018-12-03 11:00:46 +0100

    Added some possible sys.argv inputs

--- Projects/Project_3/Code/main.py
+++ Projects/Project_3/Code/main.py
@@ -21,16 +21,21 @@ np.random.seed(12)
 
 # system size
 
-num_layers = 1
-num_nodes = 300
-batch_size = 100
-epochs = 1
+num_layers = 1 # np.float(sys.argv[1])
+num_nodes = 300 # np.float(sys.argv[2])
+batch_size = 100 # np.float(sys.argv[3])
+epochs = 1 # np.float(sys.argv[4])
 
-data = input('Which dataset do you want to run, HIGGS or SUSY? [h/s]')
+data = input('Which dataset do you want to run, HIGGS or SUSY? [h/s]') # sys.argv[5]
 
+
+# input_and_hidden_activation = sys.argv[6]
+# output_activation = sys.argv[7]
+
+# dropout = sys.argv[8]
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = dataimport(data,derived_feat=False)
 
-Network(X_train,y_train,X_validate,y_validate,X_test,y_test,num_layers,num_nodes, batch_size, epochs, data)
+Network(X_train,y_train,X_validate,y_validate,X_test,y_test,num_layers,num_nodes, batch_size, epochs, data)#input_and_hidden_activation, output_activation, dropout)
 
 
