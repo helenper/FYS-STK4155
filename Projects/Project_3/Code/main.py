4a019fea40b94ec9de9fa3f8481c91f7614ef3b8
Simon Millerjord <simonmillerjord@gmail.com>
2018-12-03 11:04:16 +0100

    fixed error

--- Projects/Project_3/Code/main.py
+++ Projects/Project_3/Code/main.py
@@ -24,7 +24,7 @@ np.random.seed(12)
 num_layers = 1 # np.float(sys.argv[1])
 num_nodes = 300 # np.float(sys.argv[2])
 batch_size = 100 # np.float(sys.argv[3])
-epochs = 1 # np.float(sys.argv[4])
+epochs = 1000 # np.float(sys.argv[4])
 
 data = input('Which dataset do you want to run, HIGGS or SUSY? [h/s]') # sys.argv[5]
 
@@ -32,10 +32,10 @@ data = input('Which dataset do you want to run, HIGGS or SUSY? [h/s]') # sys.arg
 # input_and_hidden_activation = sys.argv[6]
 # output_activation = sys.argv[7]
 
-# dropout = sys.argv[8]
+# drop = sys.argv[8]
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = dataimport(data,derived_feat=False)
 
-Network(X_train,y_train,X_validate,y_validate,X_test,y_test,num_layers,num_nodes, batch_size, epochs, data)#input_and_hidden_activation, output_activation, dropout)
+Network(X_train,y_train,X_validate,y_validate,X_test,y_test,num_layers,num_nodes, batch_size, epochs, data)#input_and_hidden_activation, output_activation, drop)
 
 
