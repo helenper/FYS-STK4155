f424c29af5cd45afaf9522b9abda03ac3d42388c
Simon Millerjord <simonmillerjord@gmail.com>
2018-12-04 21:59:03 +0100

    Added possibility for different activation-functions

--- Projects/Project_3/Code/Neural_Net.py
+++ Projects/Project_3/Code/Neural_Net.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve
 import time
 
-def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers, num_nodes, batch_size, epochs, data, drop=False): # input_hidden_activation, output_activation):
+def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers, num_nodes, batch_size, epochs, data, drop=False, input_hidden_activation, output_activation):
 
 	model = tf.keras.Sequential()
 
@@ -16,18 +16,18 @@ def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers
 	hidden_initializer = tf.keras.initializers.RandomNormal(mean=0.0,stddev=0.05)
 	output_initializer = tf.keras.initializers.RandomNormal(mean=0.0,stddev=0.001)
 
-	model.add(tf.keras.layers.Dense(num_nodes, kernel_initializer=input_initializer, activation='tanh', input_dim=X_train.shape[1]))
+	model.add(tf.keras.layers.Dense(num_nodes, kernel_initializer=input_initializer, activation=input_hidden_activation, input_dim=X_train.shape[1]))
 	
 	if drop == 'True': model.add(tf.keras.layers.Dropout(0.3))
 	
 	for i in range(num_layers):
-		model.add(tf.keras.layers.Dense(num_nodes, kernel_initializer=hidden_initializer, activation='tanh'))
+		model.add(tf.keras.layers.Dense(num_nodes, kernel_initializer=hidden_initializer, activation=input_hidden_activation))
 		if drop == 'True': model.add(tf.keras.layers.Dropout(0.3))	
 
-	model.add(tf.keras.layers.Dense(y_train.shape[1], kernel_initializer=output_initializer, activation='sigmoid'))
+	model.add(tf.keras.layers.Dense(y_train.shape[1], kernel_initializer=output_initializer, activation=output_activation))
 	
 
-	earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',min_delta=0.00001,patience=3, verbose=1)
+	earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',min_delta=0.00001,patience=10, verbose=1)
 
 	#exp_learning_rate = tf.keras.callbacks.LearningRateScheduler()
 
@@ -35,7 +35,7 @@ def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers
 	model.compile(optimizer=sgd, loss='binary_crossentropy')
 
 
-	model.fit(X_train,y_train,epochs=epochs,batch_size=batch_size,validation_data=[X_validate,y_validate], callbacks=[earlystop])#,exp_learning_rate])
+	model_info = model.fit(X_train,y_train,epochs=epochs,batch_size=batch_size,validation_data=[X_validate,y_validate], callbacks=[earlystop])#,exp_learning_rate])
 
 
 	ypred = model.predict(X_test, batch_size=batch_size)
@@ -43,7 +43,7 @@ def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers
 	print('Layers: ', num_layers, 'Nodes: ', num_nodes, 'Batch size: ', batch_size)
 
 	AUC = roc_auc_score(y_test,ypred)
-	False_positive_rate, True_positive_rate = roc_curve(y_test,ypred)
+	False_positive_rate, True_positive_rate, b = roc_curve(y_test,ypred)
 
 	print('AUC: ', AUC)
 
@@ -56,8 +56,16 @@ def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers
 	file.write('Total runtime: %f' % (time.time() - start_time))
 	file.close()
 
-
-	plt.figure()
+	"""
+	plt.plot(model_info.history['val_loss'])#range(1,len(model_info.history['val_loss']) + 1), model_info.history['val_loss'])
+	plt.title('Model Loss')
+	plt.xlabel('Epoch')
+	plt.ylabel('Val Loss')
+	#plt.xticks(np.arange(1,len(model_info.history['val_loss'])+1),len(model_info.history['val_loss'])/10)
+	plt.savefig('Loss_dataset_%s_nodes%f_nlayers%f.txt' % (data,num_nodes,num_layers))
+	"""
+	
+	#plt.figure()
 
 	plt.plot(True_positive_rate,False_positive_rate)
 	plt.xlim([0.0,1.0])
@@ -65,5 +73,5 @@ def Network(X_train, y_train, X_validate, y_validate, X_test, y_test, num_layers
 	plt.xlabel('True positive rate')
 	plt.ylabel('False positive rate')
 	plt.title('ROC Curve')
-	plt.show()
-
+	plt.savefig('ROC_dataset_%s_nodes%f_nlayers%f.txt' % (data,num_nodes,num_layers))
+	
